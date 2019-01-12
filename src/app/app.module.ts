6d0e0c42040ff08a7cf30fd59092e358a0afe124
yuryklim <yuryklim@outlook.com>
2019-01-12 16:23:33 +0200

    ng g c recipes/recipe-detail --skipTests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent
+    RecipesListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
