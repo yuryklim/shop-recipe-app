970d595126212cb33ca879be7b245337439a27f1
yuryklim <yuryklim@outlook.com>
2019-01-13 13:49:41 +0200

    adding element to recipes array

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,7 +7,13 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://hips.hearstapps.com/del.h-cdn.co' +
+      '/assets/17/34/2048x1024/landscape-' +
+      '1503418862-chicken-thighs-delish.jpg?resize=1200:*')
+  ];
 
   constructor() {
   }
