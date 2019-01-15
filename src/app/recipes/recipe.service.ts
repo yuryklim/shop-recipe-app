575278019128e0946c64171ec9253abda050c903
yuryklim <yuryklim@outlook.com>
2019-01-15 19:37:27 +0200

    adding ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,24 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-      'This is simply a test',
-      'https://hips.hearstapps.com/del.h-cdn.co' +
-      '/assets/17/34/2048x1024/landscape-' +
-      '1503418862-chicken-thighs-delish.jpg?resize=1200:*'),
-    new Recipe('Another Test Recipe',
-      'This is simply a test',
-      'https://hips.hearstapps.com/del.h-cdn.co' +
-      '/assets/17/34/2048x1024/landscape-' +
-      '1503418862-chicken-thighs-delish.jpg?resize=1200:*')
+    new Recipe('Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://www.daringgourmet.com/wp-content/' +
+      'uploads/2014/03/Schnitzel-7_edited.jpg', [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://livekindlyproduction-8u6efaq1lwo6x9a.stackpathdns.com/' +
+      'wp-content/uploads/2018/08/fatburger-impossible-burger-Cropped-1.jpg', [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
