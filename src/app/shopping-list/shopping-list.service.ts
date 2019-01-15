984f4b54e59e124cf0073ef251c5fb0d6475a69f
yuryklim <yuryklim@outlook.com>
2019-01-15 18:10:42 +0200

    providing and injecting ShoppingListService

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,12 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
 
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 }
