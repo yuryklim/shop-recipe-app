32137e9da2bd6a164ae89ca2f79e22ebe9519dfa
yuryklim <yuryklim@outlook.com>
2019-01-15 17:20:50 +0200

    replacing array recipes to RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
-export class RecipeService {
+import {Recipe} from './recipe.model';
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://hips.hearstapps.com/del.h-cdn.co' +
+      '/assets/17/34/2048x1024/landscape-' +
+      '1503418862-chicken-thighs-delish.jpg?resize=1200:*'),
+    new Recipe('Another Test Recipe',
+      'This is simply a test',
+      'https://hips.hearstapps.com/del.h-cdn.co' +
+      '/assets/17/34/2048x1024/landscape-' +
+      '1503418862-chicken-thighs-delish.jpg?resize=1200:*')
+  ];
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,18 +8,6 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-      'This is simply a test',
-      'https://hips.hearstapps.com/del.h-cdn.co' +
-      '/assets/17/34/2048x1024/landscape-' +
-      '1503418862-chicken-thighs-delish.jpg?resize=1200:*'),
-    new Recipe('Another Test Recipe',
-      'This is simply a test',
-      'https://hips.hearstapps.com/del.h-cdn.co' +
-      '/assets/17/34/2048x1024/landscape-' +
-      '1503418862-chicken-thighs-delish.jpg?resize=1200:*')
-  ];
 
   constructor() {
   }
