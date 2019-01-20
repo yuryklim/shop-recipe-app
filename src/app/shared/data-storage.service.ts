0f499374c727d804dbd4e470c4446b19b32dfa82
yuryklim <yuryklim@outlook.com>
2019-01-20 20:13:10 +0200

    getting back the recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -11,5 +12,15 @@ export class DataStorageService {
   storeRecipes() {
     return this.http.put('https://recipe-book-27421.firebaseio.com/recipes.json',
       this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://recipe-book-27421.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
