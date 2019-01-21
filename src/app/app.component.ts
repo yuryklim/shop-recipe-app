8da08f377cf3fece5eb2be4c50cbcc5b3b027d58
yuryklim <yuryklim@outlook.com>
2019-01-21 14:16:49 +0200

    signing user up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
