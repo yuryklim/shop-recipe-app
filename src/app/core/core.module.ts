f577203ab0acc7b19d80b86e0067d04d27e31c08
yuryklim <yuryklim@outlook.com>
2019-01-21 22:30:30 +0200

    restructuring services to use the child injector

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {DataStorageService} from '../shared/data-storage.service';
+import {AuthService} from '../auth/auth.service';
+import {AuthGuard} from '../auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -16,8 +21,14 @@ import {AppRoutingModule} from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuard
   ]
 })
 export class CoreModule {
-  
 }
