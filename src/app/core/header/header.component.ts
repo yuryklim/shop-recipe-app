2bf9c123d3b63dc1f0214662b972289324195c2c
yuryklim <yuryklim@outlook.com>
2019-01-21 22:44:08 +0200

    preparing project for production

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -27,4 +27,8 @@ export class HeaderComponent {
   onLogout() {
     this.authService.logout();
   }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -93,4 +93,8 @@ export class RecipeEditComponent implements OnInit {
   onDeleteIngredient(index: number) {
     (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 }
