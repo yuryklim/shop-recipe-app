b079ebb85b8ba2961e37c90e24745b763258d6bd
yuryklim <yuryklim@outlook.com>
2019-01-23 19:54:42 +0200

    cleaning unused code

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from '../../shared/data-storage.service';
-import {AuthService} from '../../auth/auth.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import { Observable } from 'rxjs/Observable';
 import * as fromAuth from '../../auth/store/auth.reducers';
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
     selector: 'app-header',
@@ -15,7 +15,6 @@ export class HeaderComponent implements OnInit {
   authState: Observable<fromAuth.State>;
 
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService,
               private store: Store<fromApp.AppState>) {
   }
 
@@ -36,6 +35,6 @@ export class HeaderComponent implements OnInit {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,25 +2,16 @@ import {Injectable} from '@angular/core';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map} from 'rxjs/operators';
-import {AuthService} from '../auth/auth.service';
 import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
   constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
-              private authService: AuthService) {
+              ) {
   }
 
   storeRecipes() {
-    /*const header = new HttpHeaders().set('Authorization', 'Bearer adfafafa');*/
-    /*return this.httpClient.put(
-      'https://recipe-book-27421.firebaseio.com/recipes.json',
-      this.recipeService.getRecipes(),
-      {
-        observe: 'body',
-        params: new HttpParams().set('auth', token)
-        /!*headers: header*!/});*/
     const req = new HttpRequest(
       'PUT',
       'https://recipe-book-27421.firebaseio.com/recipes.json',
@@ -30,7 +21,6 @@ export class DataStorageService {
   }
 
   getRecipes() {
-    /*this.httpClient.get<Recipe[]>('https://recipe-book-27421.firebaseio.com/recipes.json?auth=' + token).pipe(map(*/
     this.httpClient.get<Recipe[]>(
       'https://recipe-book-27421.firebaseio.com/recipes.json',
       {observe: 'body', responseType: 'json'})
