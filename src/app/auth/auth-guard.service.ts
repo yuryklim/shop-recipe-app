0fc093f196f30b1d6411155226a2cd0d312e4ae7
yuryklim <yuryklim@outlook.com>
2019-01-23 14:50:03 +0200

    getting state access in http interceptor

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,22 +1,22 @@
-import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
 import {Injectable} from '@angular/core';
-import {AuthService} from './auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import { map } from 'rxjs/operators';
+import * as fromAuth from './store/auth.reducers';
 
 @Injectable()
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {
+  constructor(private store: Store<fromApp.AppState>) {
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+    return this.store.select('auth').pipe(map((
+      authState: fromAuth.State) => {
+        return authState.authenticated;
+      }));
   }
-
-  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isAuthenticated();
-  }
-
 }
