5ef5870750865284fa64b349e1f9e651f9b72967
yuryklim <yuryklim@outlook.com>
2019-01-25 16:47:20 +0200

    cleaning up code

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -8,78 +8,78 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthEffects {
-    @Effect()
-    authSignup = this.actions$.pipe(ofType(AuthActions.TRY_SIGNUP)).pipe(map(
-        (action: AuthActions.TrySignup) => {
-            return action.payload;
+  @Effect()
+  authSignup = this.actions$.pipe(ofType(AuthActions.TRY_SIGNUP)).pipe(map(
+    (action: AuthActions.TrySignup) => {
+      return action.payload;
+    }
+  )).pipe(switchMap(
+    (authData: { username: string, password: string }) => {
+      return fromPromise(
+        firebase.auth()
+          .createUserWithEmailAndPassword(
+            authData.username,
+            authData.password
+          ));
+    }
+  )).pipe(switchMap(
+    () => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    }
+  )).pipe(mergeMap(
+    (token: string) => {
+      this.router.navigate(['recipes']);
+      return [
+        {
+          type: AuthActions.SIGNUP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }];
+    }
+  ));
+
+  @Effect()
+  authSignin = this.actions$
+    .pipe(ofType(AuthActions.TRY_SIGNIN),
+      map(
+        (action: AuthActions.TrySignin) => {
+          return action.payload;
         }
-    )).pipe(switchMap(
-        (authData: {username: string, password: string}) => {
-            return fromPromise(
-                firebase.auth()
-                .createUserWithEmailAndPassword(
-                authData.username, 
+      ),
+      switchMap(
+        (authData: { username: string, password: string; }) => {
+          return fromPromise(
+            firebase.auth()
+              .signInWithEmailAndPassword(
+                authData.username,
                 authData.password
-            ));
+              ));
         }
-    )).pipe(switchMap(
+      ),
+      switchMap(
         () => {
-            return fromPromise(firebase.auth().currentUser.getIdToken());
+          return fromPromise(firebase.auth().currentUser.getIdToken());
         }
-    )).pipe(mergeMap(
+      ),
+      mergeMap(
         (token: string) => {
-            this.router.navigate(['recipes']);
+          this.router.navigate(['/']);
           return [
-            {
-                type: AuthActions.SIGNUP
-            },
-            {
-                type: AuthActions.SET_TOKEN,
-                payload: token
-            }];
+            {type: AuthActions.SIGNIN},
+            {type: AuthActions.SET_TOKEN, payload: token}
+          ];
         }
-    )); 
-    
-    @Effect()
-    authSignin = this.actions$
-        .pipe(ofType(AuthActions.TRY_SIGNIN),
-        map(
-            (action: AuthActions.TrySignin) => {
-                return action.payload;
-            }
-        ),
-        switchMap(
-            (authData: {username: string, password: string}) => {
-                return fromPromise(
-                    firebase.auth()
-                    .signInWithEmailAndPassword(
-                    authData.username, 
-                    authData.password
-                ));
-            } 
-        ), 
-        switchMap(
-            () => {
-                return fromPromise(firebase.auth().currentUser.getIdToken());
-            }
-        ),
-        mergeMap(
-            (token: string) => {
-                this.router.navigate(['/']);
-                return [
-                    {type: AuthActions.SIGNIN}, 
-                    {type: AuthActions.SET_TOKEN, payload: token}
-                ];
-            }
-        ));
-    @Effect({dispatch: false})
-    authLogout = this.actions$.pipe(
-        ofType(AuthActions.LOGOUT),
-        tap(
-            () => {
-                this.router.navigate(['/']);
-            }
-        ));
+      ));
+  @Effect({dispatch: false})
+  authLogout = this.actions$.pipe(
+    ofType(AuthActions.LOGOUT),
+    tap(
+      () => {
+        this.router.navigate(['/']);
+      }
+    ));
 
     constructor(private actions$: Actions, private router: Router) {}
 }
