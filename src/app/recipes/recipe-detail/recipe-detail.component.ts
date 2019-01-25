69ef0f9edb6176ec945679caf83ca9c7a7d9b824
yuryklim <yuryklim@outlook.com>
2019-01-25 13:09:10 +0200

    viewing and deleting recipes via ngrx

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,12 @@
 import {Component, Injectable, OnInit} from '@angular/core';
-import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import * as ShoppingListActions from '../../shopping-list/shopping-list.actions';
-import * as fromApp from '../../store/app.reducers';
+import {Observable} from 'rxjs/Observable';
+import * as fromRecipe from '../store/recipe.reducers';
+import {take} from 'rxjs/operators';
+import * as RecipeActions from '../store/recipe.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,35 +16,39 @@ import * as fromApp from '../../store/app.reducers';
 
 @Injectable()
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
+  recipeState: Observable<fromRecipe.State>;
   id: number;
 
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
               private router: Router,
-              private store: Store<fromApp.AppState>) { }
+              private store: Store<fromRecipe.FeatureState>) { }
 
   ngOnInit() {
     this.route.params
       .subscribe(
         (params: Params) => {
           this.id = +params['id'];
-          this.recipe = this.recipeService.getRecipe(this.id);
+          this.recipeState = this.store.select('recipes');
         }
       );
   }
 
   onAddToShoppingList() {
-    this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
+    this.store.select('recipes').pipe(take(1)).subscribe(
+      (recipeState: fromRecipe.State) => {
+        this.store.dispatch(new ShoppingListActions.AddIngredients(
+          recipeState.recipes[this.id].ingredients));
+      }
+    );
   }
 
   onEditRecipe() {
     this.router.navigate(['edit'], {relativeTo: this.route});
-    /*this.router.navigate(['../', this.id, 'edit'],  {relativeTo: this.route});*/
   }
 
   onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
+    this.store.dispatch(new RecipeActions.DeleteRecipe(this.id));
     this.router.navigate(['/recipes']);
   }
 }
--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,13 +1,14 @@
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import * as RecipeActions from './recipe.actions';
+import * as fromApp from '../../store/app.reducers';
 
-export interface FeatureState {
-    recipes: State
+export interface FeatureState extends fromApp.AppState {
+    recipes: State;
 }
 
 export interface State {
-    recipes: Recipe[]
+    recipes: Recipe[];
   }
 
 const initialState: State = {
@@ -16,18 +17,18 @@ const initialState: State = {
             'Tasty Schnitzel',
             'A super-tasty Schnitzel - just awesome!',
             'https://www.daringgourmet.com/wp-content/' +
-            'uploads/2014/03/Schnitzel-7_edited.jpg', 
+            'uploads/2014/03/Schnitzel-7_edited.jpg',
             [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
         new Recipe('Big Fat Burger',
             'What else you need to say?',
             'https://livekindlyproduction-8u6efaq1lwo6x9a.stackpathdns.com/' +
-            'wp-content/uploads/2018/08/fatburger-impossible-burger-Cropped-1.jpg', 
+            'wp-content/uploads/2018/08/fatburger-impossible-burger-Cropped-1.jpg',
             [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
         ]
     };
-  
+
 export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
-    switch(action.type) {
+    switch (action.type) {
         case(RecipeActions.SET_RECIPES):
             return {
                 ...state,
@@ -58,6 +59,6 @@ export function recipeReducer(state = initialState, action: RecipeActions.Recipe
                 recipes: oldRecipes
             };
         default:
-            return state;    
+            return state;
     }
 }
